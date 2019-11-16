27b2f17ab8396e5716907706bb5fa4dc5cf0b118
Janne Kemppainen <ext-janne.k.kemppainen@elisa.fi>
2019-11-16 13:13:48 +0200

    Add timestamps and category to SQL

--- server/schema.sql
+++ server/schema.sql
@@ -6,8 +6,11 @@ CREATE TABLE `work_order`
 `aindex` INTEGER AUTO_INCREMENT,
 `status` ENUM('new', 'details requested','ready for work','assigned','delayed','failed','finished','feedback received') NOT NULL,
 `description` TEXT NOT NULL,
+`category` TEXT NOT NULL,
 `images` TEXT,
 `worker_id` INTEGER,
+`created` DATETIME DEFAULT CURRENT_TIMESTAMP,
+`modified` DATETIME ON UPDATE CURRENT_TIMESTAMP,
 PRIMARY KEY (`aindex`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 PACK_KEYS=1;
 
